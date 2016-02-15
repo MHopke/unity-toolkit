4412b7b05ead083694b8a6cd2552f5663b1648a6
Michael Hopke <michael.hopke@gmail.com>
2016-02-15 14:39:01 -0700

    small adjustments

---
+++ Assets/Scripts/UI/OverriddenViewController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using gametheory.UI;
+using gametheory.Localization;
+
+public class OverriddenViewController : UIViewController 
+{
+	#region Overridden Methods
+	protected override void OnAwake ()
+	{
+		//TODO: Call any initialize methods here
+		//such as setting up the localization dictionary
+		base.OnAwake ();
+	}
+	protected override void OnActivate ()
+	{
+		#if UNITY_ANDROID
+		Screen.fullScreen = false;
+		#endif
+		base.OnActivate();
+
+		//TODO: perform any startup logic here
+		//such as determining if a login, or home page 
+		//be displayed
+	}
+	#endregion
+}
