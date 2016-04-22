fe338233725c5e44b43f1bdb7c7bb86930f77c78
Michael Hopke <michael.hopke@gmail.com>
2016-04-22 11:43:40 -0600

    line test

---
+++ Assets/LineTest/LineTest.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineTest : MonoBehaviour 
+{
+	#region Public Vars
+	public LineRenderer Prefab;
+	#endregion
+
+	#region Private Vars
+	LineRenderer _line = null;
+	#endregion
+
+	#region Unity MEthods
+	void Awake()
+	{
+		Point.clicked += PointClicked;
+	}
+	void OnDestroy()
+	{
+		Point.clicked -= PointClicked;
+	}
+	#endregion
+
+	#region Event Listeners
+	void PointClicked(Point point)
+	{
+		if(_line == null)
+		{
+			_line = (LineRenderer)GameObject.Instantiate(Prefab,Vector3.zero,Quaternion.identity);
+
+			_line.SetPosition(0,point.transform.position);
+			_line.SetPosition(1,point.transform.position);
+
+			point.LineIndex = 0;
+			point.Line = _line;
+		}
+		else
+		{
+
+			point.Line = _line;
+			point.LineIndex = 1;
+			_line.SetPosition(1,point.transform.position);
+			_line = null;
+		}
+	}
+	#endregion
+}
---
+++ Assets/LineTest/Point.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Point : MonoBehaviour 
+{
+	#region Events
+	public static event System.Action<Point> clicked;
+	#endregion
+
+	#region Public Vars
+	public int LineIndex;
+	public LineRenderer Line;
+	#endregion
+
+	#region PRviate Vars
+	Vector3 _dragPos;
+	#endregion
+
+	#region UI Methods
+	public void OnPointerDown(BaseEventData data)
+	{
+		if(Line)
+			return;
+		
+		if(clicked != null)
+			clicked(this);
+	}
+	public void OnDrag(BaseEventData data)
+	{
+		if(Line == null)
+			return;
+
+		PointerEventData pointData = data as PointerEventData;
+
+		_dragPos = Camera.main.ScreenToWorldPoint(pointData.position);
+		_dragPos.z = transform.position.z;
+
+		transform.position = _dragPos;
+
+		Line.SetPosition(LineIndex,transform.position);
+	}
+	#endregion
+}
